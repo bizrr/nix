df399ca5c79d545f9ace920d9bc7c375a886f561
Roy Långsjö <roylangsjo@gmail.com>
2024-12-17 02:02:25 +0200

    first commit 8)

---
+++ modules/programs/git.nix
@@ -0,0 +1,18 @@
+{ programs, ... }: {
+  programs.git = {
+    enable = true;
+    userName = "Roy Långsjö";
+    userEmail = "roylangsjo@gmail.com";
+
+    aliases = {
+      co = "checkout";
+    };
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+      pull.rebase = true;
+    };
+	
+  };
+}
